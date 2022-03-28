29287b572d5c683f947e71364f65191817af234a
Nick Dodge <chetdodge@gmail.com>
2022-03-28 19:57:32 +0100

    Added error checking for inputs

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "stdlib.h"
 #include "time.h"
 #include "stdio.h"
+#include "float.h"
 
 /* Defines */
-#define RAND_RANGE_MIN (-5000)
-#define RAND_RANGE_MAX (5000)
-#define INPUT_ARRAY_SIZE (10000)
+#define RAND_RANGE_MIN (-500000)
+#define RAND_RANGE_MAX (500000)
+#define INPUT_ARRAY_SIZE (100000)
 
 /* Function Prototypes */
 double iterative_average(double *input_value_array, long long unsigned int input_value_array_size);
@@ -28,25 +29,45 @@ int main() {
     averaged_value = iterative_average(array_of_doubles, INPUT_ARRAY_SIZE);
 
     // Print result
-    printf("%f", averaged_value);
+    if(averaged_value == DBL_MAX){
+        printf("Error occurred, please check configuration");
+    } else {
+        printf("%f", averaged_value);
+    }
 }
 
 
 /*
  * Iterative Average Function Definition
  *
- * Find the average of an array of double floating point inputs using an iterative approach
+ * Find the average of an array of double floating point inputs using an iterative approach. We need to use iterative
+ * approach because the number of elements is arbitrary which could lead to overflow issues.
  *
- * Returns the calculated iterative average
+ * https://www.heikohoffmann.de/htmlthesis/node134.html
+ * next_average = current_average + ( (1 / (index + 1)) * (input_value - current_average) )
+ *
+ * Inputs:
+ * input_value_array      --- an array of doubles of arbitrary size to be averaged
+ * input_value_array_size --- size of array to be averaged, can be up to ULLONG_MAX
+ *
+ * Outputs:
+ * Returns the calculated iterative average, DBL_MAX if there is an error
  */
 double iterative_average(double *input_value_array, long long unsigned int input_value_array_size) {
     double current_average = 0.0;
 
+    // Check inputs to ensure valid
+    if(input_value_array == NULL || input_value_array_size == 0){
+        return DBL_MAX;
+    }
+
+    // Iterate through list, calculating average as we go
     for (long long unsigned int index = 0; index < input_value_array_size; index++) {
         // next_average = current_average + ((1 / (index           + 1)) * (input_value              - current_average))
         current_average = current_average + ((1 / (((double)index) + 1)) * (input_value_array[index] - current_average));
     }
 
+    // Return calculated average
     return current_average;
 }
 
@@ -54,9 +75,15 @@ double iterative_average(double *input_value_array, long long unsigned int input
 /*
  * Random Range Function Definition
  *
- * Outputs a random double value within the range provided
+ * Output:
+ * Random double value within the range provided
  */
 double rand_range(double min, double max) {
+    // Ensure min is less than max
+    if(min > max){
+        return DBL_MAX;
+    }
+
     double random = ((double) rand()) / RAND_MAX;
     double range = (max - min) * random;
     double number = min + range;
